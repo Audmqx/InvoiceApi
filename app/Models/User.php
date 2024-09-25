84c6965fa4d61d1c5c4cff27be6699343841f5f8
MaximPixodeo <iangaev.m@live.fr>
2024-09-25 21:30:23 +0200

    Add Larastan

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable; // @phpstan-ignore-line
 
     /**
      * The attributes that are mass assignable.
